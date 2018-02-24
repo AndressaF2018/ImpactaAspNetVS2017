48bf28a65a4a38e83facae579a073578e4dbacb0
Andressa Falasco <andressafalasco@gmail.com>
2018-02-24 16:42:26 -0300

    go
    go

---
+++ Capitulo02.Repeticao.Testes/RepeticaoTeste.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Capitulo02.Repeticao.Testes
+{
+    [TestClass]
+    public class RepeticaoTeste
+    {
+        [TestMethod]
+        public void ForAninhadoTeste()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <=10; j++)
+                {
+                    Console.WriteLine($" {i} * {j} = {i *j}");
+                }
+                Console.WriteLine(new string('-', 50));
+            }
+        }
+    }
+}
---
+++ Capitulo2.Tabuada/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Capitulo2.Tabuada
+{
+    partial class TabuadaForm
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabuadaListBox = new System.Windows.Forms.ListBox();
+            this.tabuadaTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tabuadaListBox
+            // 
+            this.tabuadaListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabuadaListBox.FormattingEnabled = true;
+            this.tabuadaListBox.Location = new System.Drawing.Point(1, 35);
+            this.tabuadaListBox.Name = "tabuadaListBox";
+            this.tabuadaListBox.Size = new System.Drawing.Size(416, 238);
+            this.tabuadaListBox.TabIndex = 0;
+            // 
+            // tabuadaTextBox
+            // 
+            this.tabuadaTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabuadaTextBox.Location = new System.Drawing.Point(1, 9);
+            this.tabuadaTextBox.MaxLength = 4;
+            this.tabuadaTextBox.Name = "tabuadaTextBox";
+            this.tabuadaTextBox.Size = new System.Drawing.Size(418, 20);
+            this.tabuadaTextBox.TabIndex = 1;
+            this.tabuadaTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tabuadaTextBox_KeyPress);
+            // 
+            // TabuadaForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 293);
+            this.Controls.Add(this.tabuadaTextBox);
+            this.Controls.Add(this.tabuadaListBox);
+            this.Name = "TabuadaForm";
+            this.Text = "Tabuada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox tabuadaListBox;
+        private System.Windows.Forms.TextBox tabuadaTextBox;
+    }
+}
+
